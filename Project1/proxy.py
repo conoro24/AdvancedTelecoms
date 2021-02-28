191f91c20a214411eeda83733c78c9e780c225b4
conoro24 <conorbrian99@gmail.com>
2021-02-28 16:28:13 -0500

    Block and Unblock functions

--- Project1/proxy.py
+++ Project1/proxy.py
@@ -9,3 +9,35 @@ DEBUG = True
 blocked = {}
 cache = {}
 timings = {}
+
+
+def tkinter():
+    console = tk.Tk()
+    block = Entry(console)
+    block.grid(row=0, column=0)
+    unblock = Entry(console)
+    unblock.grid(row=1, column=0)
+
+    def block_url():
+        val = block.get()
+        temp = blocked.get(val)
+        if temp is None:
+            blocked[val] = 1
+            print("Successfully Blocked: " + val)
+        else:
+            print("This website is already blocked...")
+
+    block_button = Button(console, text="Block URL", command=block_url)
+    block_button.grid(row=0, column=1)
+
+    def unblock_url():
+        val = unblock.get()
+        temp = blocked.get(val)
+        if temp is None:
+            print("The URL is not blocked: " + val)
+        else:
+            blocked.pop(val)
+            print("This website is unblocked: " + val)
+
+    unblock_button = Button(console, text="Unblock URL", command=unblock_url)
+    unblock_button.grid(row=1, column=1)
