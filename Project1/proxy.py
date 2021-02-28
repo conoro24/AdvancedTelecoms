51f5748ea9c783fb75c4bc0f4fa4822d2037febb
conoro24 <conorbrian99@gmail.com>
2021-02-28 16:06:20 -0500

    Constraints

--- Project1/proxy.py
+++ Project1/proxy.py
@@ -1,4 +1,11 @@
 import os, sys, thread, socket, time
-import Tkinter as tk 
+import Tkinter as tk
 from TKinter import *
 
+# CONSTRAINTS
+BACKLOG = 200
+MAX_DATA_RECV = 4096
+DEBUG = True
+blocked = {}
+cache = {}
+timings = {}
